eaa72db98df65bde0f05496ccf26d15da1f6c4e7
gabsoares <soaresgabriel68@gmail.com>
2023-06-03 00:12:06 -0300

    Add project files.

---
+++ ListaDeExercicios6/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] numbers = new int[4];
+
+Console.WriteLine("Digite quatro números inteiros e no final te informaremos qual o maior e o menor número digitado");
+numbers[0] = Convert.ToInt32(Console.ReadLine());
+numbers[1] = Convert.ToInt32(Console.ReadLine());
+numbers[2] = Convert.ToInt32(Console.ReadLine());
+numbers[3] = Convert.ToInt32(Console.ReadLine());
+
+int smallerNumber, higherNumber;
+
+Array.Sort(numbers);
+
+smallerNumber = numbers[0];
+higherNumber = numbers[numbers.Length - 1];
+
+Console.WriteLine($"O maior número digitado foi o {higherNumber} e o menor foi o {smallerNumber}");
